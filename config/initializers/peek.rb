aef825cf8fc09b1e821b00a515f6cb38c25e425b
BGMP <jose@bgmp.cl>
2022-08-13 02:13:04 -0400

    Update Peek-Git's stuff

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Peek.into Peek::Views::Git
+Peek.into Peek::Views::Git, :nwo => 'BGMP/BGM'
 Peek.into Peek::Views::PerformanceBar
 Peek.into Peek::Views::Mysql2
-Peek.into Peek::Views::Rblineprof
+#Peek.into Peek::Views::Rblineprof

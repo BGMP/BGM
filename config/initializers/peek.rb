372b06e8fc69104acfeebe29a73a63634f13d99c
BGMP <jose@bgmp.cl>
2022-09-03 20:14:14 -0400

    Update peek initializer

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
-
-Peek.into Peek::Views::Git, :nwo => 'BGMP/BGM'
-Peek.into Peek::Views::PerformanceBar
-Peek.into Peek::Views::Mysql2
-Peek.into Peek::Views::Rblineprof
+if BGM::Application.bgm_role == 'bgm'
+  Peek.into Peek::Views::PerformanceBar
+  Peek.into Peek::Views::Mysql2
+  Peek.into Peek::Views::Rblineprof
+end

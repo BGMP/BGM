280013bd400f689ffe4da7e89058fa586eee53fe
BGMP <jose@bgmp.cl>
2022-10-16 04:37:08 -0300

    Fix pretty_time()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
   # @param datetime [DateTime]
   # @return [String] Pretty date string (i.e: Thursday 21st, 2022 - 06:32 AM)
   def pretty_time(datetime)
-    datetime.strftime("%A #{datetime.day.ordinalize}, %Y - %H:%M %p")
+    datetime.strftime("%B #{datetime.day.ordinalize}, %Y - %H:%M %p")
   end
 
   # @param content [String] Content

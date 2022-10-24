3940e43705838e4a79104940df05f9352b4886a7
BGMP <jose@bgmp.cl>
2022-10-23 23:06:34 -0300

    Add web role to capistrano deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-server 'network.bgm.cl', :user => 'deploy', :roles => 'app db', :primary => true
+server 'network.bgm.cl', :user => 'deploy', :roles => 'web app db', :primary => true
 
 set :ssh_options, {
   :keys => %w(C:/Users/BGM/.ssh/elmedievo),
